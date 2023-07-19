aff78d809eb0a5a5a48520d3784bcccace4938e5
Richard McCormack <brick@brick.codes>
2023-07-18 22:09:49 -0400

    add a heuristic to speed up branch+bound algorithm

--- letterboxed_cli/src/main.rs
+++ letterboxed_cli/src/main.rs
@@ -3,7 +3,8 @@ use letterboxed_lib::SolverState;
 fn main() {
    //let board = ['S', 'O', 'F', 'T', 'I', 'N', 'D', 'G', 'A', 'R', 'U', 'H'];
    //let board = ['L', 'A', 'B', 'L', 'A', 'B', 'L', 'A', 'B', 'L', 'A', 'B'];
-   let board = ['E', 'A', 'B', 'D', 'R', 'U', 'S', 'I', 'T', 'O', 'M', 'X'];
+   //let board = ['E', 'A', 'B', 'D', 'R', 'U', 'S', 'I', 'T', 'O', 'M', 'X'];
+   let board = ['T', 'A', 'M', 'U', 'R', 'D', 'I', 'B', 'Q', 'E', 'N', 'O'];
    let mut s = SolverState::setup(board);
 
    while let Some(ans) = s.next_solution() {
--- letterboxed_lib/src/lib.rs
+++ letterboxed_lib/src/lib.rs
@@ -22,6 +22,7 @@ pub struct SolverState {
    best_solution: Option<SolutionState>,
    board: GameGrid,
    stack: Vec<SolutionState>,
+   attempted_heuristic: bool,
 }
 
 fn word_can_be_made(position: Option<usize>, mut remaining_word: Vec<char>, board: &GameGrid) -> bool {
@@ -43,6 +44,67 @@ fn word_can_be_made(position: Option<usize>, mut remaining_word: Vec<char>, boar
    }
 
    false
+}
+
+fn word_path(position: Option<usize>, mut remaining_word: Vec<char>, board: &GameGrid, path: Vec<usize>) -> Option<Vec<usize>> {
+   let next_letter = match remaining_word.pop() {
+      Some(nl) => nl,
+      None => return Some(path),
+   };
+
+   let reachable_letters = if let Some(pos) = position {
+      reachable(pos)
+   } else {
+      &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+   };
+
+   for possible_dest in reachable_letters.iter() {
+      if board[*possible_dest] == next_letter {
+         let mut next_path = path.clone();
+         next_path.reserve_exact(1);
+         next_path.push(*possible_dest);
+
+         let final_path = word_path(Some(*possible_dest), remaining_word.clone(), board, next_path);
+         if final_path.is_some() {
+            return final_path;
+         }
+      }
+   }
+
+   None
+}
+
+fn heuristic_solution(dict: &Trie<BString, ()>, board: &GameGrid) -> Option<SolutionState> {
+   let mut visited: u16 = 0xF000;
+   let mut words: Vec<String> = vec![];
+
+   let mut position = None;
+
+   let mut flat_dict_again: Vec<Vec<char>> = dict.keys().map(|x| x.as_str().chars().collect()).collect();
+
+   while visited != 0xFFFF {
+      flat_dict_again.sort_by_key(|x| {
+         let mut chars: Vec<char> = x.clone();
+         chars.sort();
+         chars.dedup();
+         chars.len()
+      });
+
+      if let Some(i) = flat_dict_again.iter().enumerate().rev().find(|x| word_can_be_made(position, x.1.clone(), board)).map(|x| x.0) {
+         let greedy_best_next_word = flat_dict_again.swap_remove(i);
+         words.push(greedy_best_next_word.iter().collect());
+         let path = word_path(position, greedy_best_next_word, board, vec![]).unwrap();
+         for dest in path.iter().copied() {
+            visited |= 1 << dest as u16;
+         }
+         position = Some(*path.last().unwrap());
+      } else {
+         return None;
+      }
+   }
+
+   let total_letters = words.iter().map(|x| x.chars().count()).sum();
+   Some(SolutionState { cur_sequence: String::new(), words, position: position.unwrap(), visited: 0xFFFF, last_char: '\0', total_letters })
 }
 
 impl SolverState {
@@ -78,10 +140,20 @@ impl SolverState {
             total_letters: 0,
             last_char: '\0',
          }],
+         attempted_heuristic: false,
       }
    }
 
    pub fn next_solution(&mut self) -> Option<String> {
+      if !self.attempted_heuristic {
+         self.attempted_heuristic = true;
+         if let Some(h) = heuristic_solution(&self.dict, &self.board) {
+            let solution_str = h.words.join(", ");
+            self.best_solution = Some(h);
+            return Some(solution_str);
+         }
+      }
+
       while let Some(solution) = self.stack.pop() {
          // Bound
          if let Some(bs) = self.best_solution.as_ref() {
